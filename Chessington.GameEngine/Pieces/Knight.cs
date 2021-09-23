c7526ddd804e52554de9dc20678052663c04628f
Joseph Bartram <J.Bartram@ucas.ac.uk>
2021-09-23 16:15:24 +0100

    Red 6 has been completed by spagetty code

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,35 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square newPosition;
+            Square currentPosition = board.FindPiece(this);
+            var availableSquares = new List<Square>();
+
+            newPosition = Square.At(currentPosition.Row - 1, currentPosition.Col + 2);
+            availableSquares.Add(newPosition);
+
+            newPosition = Square.At(currentPosition.Row - 1, currentPosition.Col - 2);
+            availableSquares.Add(newPosition);
+
+            newPosition = Square.At(currentPosition.Row + 1, currentPosition.Col + 2);
+            availableSquares.Add(newPosition);
+
+            newPosition = Square.At(currentPosition.Row + 1, currentPosition.Col - 2);
+            availableSquares.Add(newPosition);
+
+            newPosition = Square.At(currentPosition.Row - 2, currentPosition.Col + 1);
+            availableSquares.Add(newPosition);
+
+            newPosition = Square.At(currentPosition.Row - 2, currentPosition.Col - 1);
+            availableSquares.Add(newPosition);
+
+            newPosition = Square.At(currentPosition.Row + 2, currentPosition.Col + 1);
+            availableSquares.Add(newPosition);
+
+            newPosition = Square.At(currentPosition.Row + 2, currentPosition.Col - 1);
+            availableSquares.Add(newPosition);
+
+            return availableSquares;
         }
     }
 }
