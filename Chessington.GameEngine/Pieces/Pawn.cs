05a5bcb36227aa2011df88a71badc794ba0b44a9
Joseph Bartram <J.Bartram@ucas.ac.uk>
2021-09-23 10:48:25 +0100

    Got red 01 working

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,35 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square newPosition;
+            Square currentPosition = board.FindPiece(this);
+            var availableSquares = new List<Square>();
+            if (Player == Player.White)
+            {
+                newPosition = Square.At(currentPosition.Row - 1, currentPosition.Col);
+                availableSquares.Add(newPosition);
+            }
+            else
+            {
+                newPosition = Square.At(currentPosition.Row + 1, currentPosition.Col);
+                availableSquares.Add(newPosition);
+            }
+
+            if (currentPosition.Row == 6 || currentPosition.Row == 1)
+            {
+                if (Player == Player.White)
+                {
+                    newPosition = Square.At(currentPosition.Row - 2, currentPosition.Col);
+                    availableSquares.Add(newPosition);
+                }
+                else
+                {
+                    newPosition = Square.At(currentPosition.Row + 2, currentPosition.Col);
+                    availableSquares.Add(newPosition);
+                }
+            }
+
+            return availableSquares;
         }
     }
 }
